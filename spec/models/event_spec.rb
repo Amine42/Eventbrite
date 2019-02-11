909241b3bc942ecf13286abdc2646d80f3250f25
Malik42 <malik.ahres@hotmail.fr>
2019-02-11 16:48:34 +0100

    finish rspec 2 to 3

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Event, type: :model do
 		Event.create(title: "Conference")
 		expect(Event.count).to eq(1)
 	end
+
+	it "should create a event related to the attendance" do 
+		event = Event.new(title: "Evenement")
+		event.attendances << Attendance.new(event_id: "1")
+		event.save
+		expect(Attendance.count).to eq(1)
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
 		User.create(email: "blablabla@gmail.com")
 		expect(User.count).to eq(1)
 	end
+
+	it "should create a user related to the attendance" do 
+		user = User.new(email: "blablabla@gmail.com")
+		user.attendances << Attendance.new(user_id: "1")
+		user.save
+		expect(Attendance.count).to eq(1)
+	end
 end
