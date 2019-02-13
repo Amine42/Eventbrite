ee0ce9f5c6501c0190b56eac8fec9cab19e0d7be
Amine42 <amine.bouraghda@gmail.com>
2019-02-13 23:10:26 +0100

    generate attendance controller

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "events/index.html.erb", type: :view do
   it "displays the widget" do
-    assign(:event, Event.create!(:title => "Of Human Bondage"))
+    assign(:event, Event.create!(:title => "Of Human Bondage", :start_date => DateTime.now + 9999, :duration => 50, :description => "je test tes teste tettet et tet et et ett et", :price => 90, :location => "firminy"))
 
     render :template => "events/index.html.erb"
 
