5bf3b6e908dc4f777981eef18e9d6da3bda482b8
Amine42 <amine.bouraghda@gmail.com>
2019-02-14 04:54:56 +0100

    edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,17 @@ class UsersController < ApplicationController
 
   def edit
     # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+    @user = User.find(params[:id])
   end
 
   def update
     # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
     # pour info, le contenu de ce formulaire sera accessible dans le hash params
     # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+    @user = User.find(params[:id])
+    post_params = params.require(:user).permit(:first_name, :last_name, :description)
+    @user.update(post_params)
+    redirect_to user_path(@user.id)
   end
 
   def destroy
